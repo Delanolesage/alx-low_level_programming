51e6b0f461ea6287e48f670f50dff88febd82079
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-11 16:01:17 +0100

    Create 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,10 +8,11 @@ int main(void)
 {
 	int n = 0;
 
-	while (n < 10)
+	while (n < 100)
 	{
-		putchar(n + '0');
-		if (n < 9)
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+		if (n < 99)
 		{
 			putchar(',');
 			putchar(' ');
