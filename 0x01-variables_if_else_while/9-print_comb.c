01b7bf5658b34fc04b76661939e415961a1826cd
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-03-11 15:02:23 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		putchar(' ,');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
