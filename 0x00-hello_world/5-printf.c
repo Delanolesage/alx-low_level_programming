f03b3cbba3ad070ea81911f10882ba6505374957
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-11 03:32:31 +0100

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is apiece of art,");
+	return (0);
+}
