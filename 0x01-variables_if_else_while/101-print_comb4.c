b118c8c6fedf8cb3124ee9d7c073ca70139dbc5d
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-11 20:48:53 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,7 +22,7 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				if (i != 7 && j != 8 && k != 9)
+				if (((i * 100) + (j * 10) + k) != 789)
 				{
 					putchar(',');
 					putchar(' ');
