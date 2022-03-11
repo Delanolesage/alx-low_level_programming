1b1ce47533c68cf72dda29709ce274866f5218c0
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-11 18:54:25 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int n = 0;
-	int i;
+	int i, j, k;
 
 	while (n <= 999)
 	{
@@ -16,10 +16,12 @@ int main(void)
 		{
 			if (i / 100 % 10 > i % 10)
 				break;
-			i /= 10;
+			j = (n - ((n / 100) * 100)) / 10; // 8 pour 987
+			k = n - ((n / 10) *10); // 7 pour 987
+			i /= 100; // 9 pour 987
 			if (i == 0)
 			{
-				if ((n % 11) != 0 && n != 11)
+				if (i != j && i != k && j != k)
 				{
 					putchar((n / 100) + '0');
 					putchar((n / 10) + '0');
