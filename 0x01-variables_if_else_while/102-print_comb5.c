bc09987124b849997f0d45e603f96c980363c373
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-12 01:53:53 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,26 +6,26 @@
  */
 int main()
 {
-    int a, b, c;
-    
+	int a, b, c;
+	
 	for (int i = 0; i <= 9; i++)
 	{
-	    for (int j = 0; j <= 9; j++)
-	    {
-	        for (int k = 0; k <= 9; k++)
-	        {
-	            for (int l = 1; l <= 9; l++)
-	            {
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(' ');
-			putchar(k + '0');
-			putchar(l + '0');
-			putchar(',');
-			putchar(' ');
-		    }
+		for (int j = 0; j <= 9; j++)
+		{
+			for (int k = 0; k <= 9; k++)
+			{
+				for (int l = 1; l <= 9; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-	    }
 	}
 	putchar('\n');
 	return 0;
