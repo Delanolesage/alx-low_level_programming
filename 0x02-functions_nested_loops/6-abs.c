20a836cf8d2237fc9a15da6ba817af28213f2c6c
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-15 23:48:24 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,17 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+		n *= -1;
 	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	return n;
 }
