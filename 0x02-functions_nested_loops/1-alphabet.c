1c1d22819fccc899db599aeaa9ddb46b197e8752
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-15 21:08:12 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,3 +9,21 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
+
+/**
+ *
+ * print_alphabet - print the alphabet
+ *
+ * void function: nothing to return
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a < 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
