ffee51b715af721021c24337010644c882cecb9b
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-15 18:28:52 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,7 +13,7 @@ int main(void)
 	char ch[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
 	int length = sizeof(ch) / sizeof(char);
 	int i;
-	
+
 	for (i = 0; i < length; i++)
 		_putchar(ch[i]);
 	return (0);
