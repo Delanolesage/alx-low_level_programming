ac3cf4626625fe2aca4775d4002bee4f71d8ec9b
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-16 21:19:52 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,13 +12,13 @@ int main(void)
 	long int f_2 = 1;
 	long int fiboSum = f_1 + f_2;
 
-	for (i = 1; i <= 100; ++i)
+	for (i = 1; i <= 50; ++i)
 	{
 		printf("%ld", fiboSum);
 		f_1 = f_2;
 		f_2 = fiboSum;
 		fiboSum = f_1 + f_2;
-		if (i != 100)
+		if (i != 50)
 			printf(", ");
 	}
 	return (0);
