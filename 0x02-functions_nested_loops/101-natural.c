29a2c4400c57fe6cfb16210a81feed4f82809f90
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-16 19:49:38 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,20 @@
-# include "main.h"
+# include <stdio.h>
 
 /**
  * main - check the code.
  *
- * Return: Always 0.
+ * Return: Always 0 if success.
  */
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+	int i;
+	int fiboSum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			fiboSum += i;
+		}
+	}
 	return (0);
 }
