6e64e4e2bcd7ad6aea2256e9ce0115acabb387c3
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-16 23:03:14 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,12 +15,14 @@ int main(void)
 
 	for (i = 1; i <= 50; ++i)
 	{
+		while (fiboSum <= stop) {
 		f_1 = f_2;
 		f_2 = fiboSum;
 		fiboSum = f_1 + f_2;
 		if (fiboSum >= stop)
 		{
 			printf("%ld ", fiboSum);
+		}
 		}
 	}
 	printf("\n");
