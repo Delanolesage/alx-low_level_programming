783e73b24a31eab60ba243d6f00ca6b47fd4a3ce
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-16 20:12:45 +0100

    Update 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0 if success.
+ */
+int main(void)
+{
+	int i;
+	int fiboSum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			fiboSum += i;
+		}
+	}
+	printf("%d\n", fiboSum);
+	return (0);
+}
