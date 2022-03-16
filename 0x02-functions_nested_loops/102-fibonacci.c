7e81df04f67705a7545bf981775a228c3fc68f17
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-16 21:15:31 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int i;
-	int f_1 = 0;
-	int f_2 = 1;
-	int fiboSum = f_1 + f_2;
+	long int f_1 = 0;
+	long int f_2 = 1;
+	long int fiboSum = f_1 + f_2;
 
 	for (i = 1; i <= 100; ++i)
 	{
