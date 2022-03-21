52d8779b3c9203c00feaca46f1cb69be6f3bc763
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-03-21 23:54:07 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: string to print
+ * void: nothing to return
+ */
+void print_rev(char *s)
+{
+	int i;
+	int len = strlen(s);
+
+	for (i = len - 1; i <= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
