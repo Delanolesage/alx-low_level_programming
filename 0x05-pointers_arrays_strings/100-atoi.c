44a33d2d33a9b46ec146ae5003044f9768444a21
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-22 15:27:59 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <string.h>
-#include <math.h>
+#include <stdlib.h>
 
 /**
  * _atoi - converts a string to an integer.
@@ -9,40 +9,49 @@
  */
 int _atoi(char *s)
 {
-	int i, j, k, len, minus, plus, countInt, result;
-	char *res;
+	int i, j, k, l, minus, countInt, result;
 
-	len = strlen(s);
-	plus = 0;
 	minus = 0;
 	countInt = 0;
-	res = " ";
+	l = 0;
 
-	for (i = 0; i < len; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] <= '9' && s[i] >= '0')
+			countInt++;
+	}
+
+	int size = countInt - 1;
+
+	char *str = (char *)malloc(sizeof(char)*size);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			minus++;
+		}
 		if (s[i] == ';')
 			break;
-		if (s[i] == '+')
-			plus++;
-		if (s[i] == '-')
-			minus++;
-		if (s[i] <= 9 || s[i] >= 0)
+		if (s[i] <= '9' && s[i] >= '0')
 		{
-			res[countInt] = s[i];
-			countInt++;
+			str[l++] = s[i];
 		}
 	}
+
 	if(countInt == 0)
 		result = 0;
 	else
 	{
-		for (j = 0; j <= countInt - 2; j++)
+		for (j = 0; j < l; j++)
 		{
 			int p = 1;
 
-			for (k = countInt - 2 - j; k > 0; k--)
+			for (k = l - 1 + j; k < l; k++)
+			{
 				p *= 10;
-			result += res[j] * p;
+			}
+			result += (str[j] - '0') * p;
 		}
 		if (minus % 2 != 0)
 			result *= -1;
