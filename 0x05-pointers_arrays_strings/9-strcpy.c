9854a460141dc91fbceedef7c20d6597cfe7f9a3
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-03-22 03:59:40 +0100

    Create 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int len = strlen(src);
 
-
-	for (i = 0; i < len; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
