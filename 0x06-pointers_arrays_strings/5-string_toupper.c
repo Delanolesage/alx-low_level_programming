611ac6629214626da29f412338e52cfc1537708f
root <delanolesage@gmail.com>
2022-03-23 16:16:03 -0700

    Create 5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,7 +4,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase.
  * @s: the string
- * 
+ *
  * Return: the changed string
  */
 char *string_toupper(char *s)
@@ -15,7 +15,7 @@ char *string_toupper(char *s)
 
 	for (i = 0; i <  len && s[i] != '\0'; i++)
 	{
-		if(s[i] >= 97 && s[i] <= 122)
+		if (s[i] >= 97 && s[i] <= 122)
 			s[i] -= 32;
 	}
 	return (s);
