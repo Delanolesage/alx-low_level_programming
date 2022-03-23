cbca7666355fa432bc209cf56edbe9d98dbfa250
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-03-24 00:08:30 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: the string
+ * 
+ * Return: the changed string
+ */
+char *string_toupper(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+
+	for (i = 0; i <  len && s[i] != '\0'; i++)
+	{
+		if(s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+	}
+	return (s);
+}
