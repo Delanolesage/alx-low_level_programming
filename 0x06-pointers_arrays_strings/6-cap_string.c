e5f86a808e0ed9e9018c2e22dca84e13a9547e42
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-03-24 00:42:30 +0100

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: the string
+ *
+ * Return: the changed string
+ */
+char *cap_string(char *s)
+{
+	int i, j, len;
+	int sep[13];
+
+	len = strlen(s);
+	sep = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};}
+
+	for (i = 0; i <  len && s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j] && (s[i + 1] >= 97 && s[i+1] <= 122)
+				s[i] -= 32;
+		}
+	}
+	return (s);
+}
