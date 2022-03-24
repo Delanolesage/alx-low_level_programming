757589d3263d3468f9b1251bce93b649e16510c8
root <delanolesage@gmail.com>
2022-03-23 18:59:20 -0700

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,26 +2,27 @@
 #include <string.h>
 
 /**
- * _strncmp - compares two strings.
- * @s1: the firdt string
- * @s2 the second string
- * 
+ * _strcmp - compares two strings.
+ * @s1: the first string
+ * @s2: the second string
+ *
  * Return: the copied string
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i, status;
 
+	i = 0;
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if(s1[i] != s2[i])
+		if (s1[i] != s2[i])
 		{
 			status = s1[i] - s2[i];
-			return status;
+			break;
 		}
 		else
 		{
-			status = 0;
+			return (0);
 		}
 	}
 	return (status);
