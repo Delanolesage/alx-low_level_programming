6f446aeaa5c8af3027fff639713c0ab74ab132ce
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-03-24 23:29:30 +0100

    Update 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: the string
+ *
+ * Return: the encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char letters1[] = {"ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz"};
+	char letters2[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters1[j] != '\0'; j++)
+		{
+			if (s[i] == letters1[j])
+			{
+				s[i] = letters2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
