9702d2e8e707070a4228d0b084f3ab755a10c8f2
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-24 23:33:14 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,7 +9,7 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char letters1[] = {"ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz"};
+	char letters1[] = {"ABCDEFGHIJKLMNOPQRSTUVwXYZabcdefghijklmnopqrstuvwxyz"};
 	char letters2[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
 
 	for (i = 0; s[i] != '\0'; i++)
