c6c27c6791bb4d9a89705d97cad9adc0dda7e51d
root <delanolesage@gmail.com>
2022-03-23 18:34:41 -0700

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,8 @@ char *_strncpy(char *dest, char *src, int n)
 		if (n < lenDest)
 			dest[i] = src[i];
 	}
-	for ( ; i < n; i++)
-		 dest[i] = '\0';
+	for (i; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
