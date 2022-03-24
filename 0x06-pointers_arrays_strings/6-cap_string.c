46760f4ab412d4f40a2eefe0bb5365bb27666e80
root <delanolesage@gmail.com>
2022-03-24 15:05:47 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,11 +16,12 @@ char *cap_string(char *s)
 
 	for (i = 0; i <  len && s[i] != '\0'; i++)
 	{
+		if (i == 0 && s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == sep[j])
-			       if (s[i + 1] >= 97 && s[i + 1] <= 122))
-				       	s[i + 1] -= 32;
+			if ((s[i] == sep[j]) && (s[i + 1] >= 97 && s[i + 1] <= 122))
+				s[i + 1] -= 32;
 		}
 	}
 	return (s);
