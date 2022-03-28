84355efba85b20f61eb5ad8a931929606ec5949f
root <delanolesage@gmail.com>
2022-03-28 07:38:55 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n && dest[i] != '\0' && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
 
 	return (dest);
 }
