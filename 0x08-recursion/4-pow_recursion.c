915a0bf2efc66e5b411ae6c893f84e8a3baa1087
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-03-31 23:43:57 +0200

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - returns x raised to power of y
+ * @x: the value to power
+ * @y: the power
+ *
+ * Return: the power of the value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
