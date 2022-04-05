7dbffe160ca53de13c7628162b3a17abb8961267
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-06 01:03:58 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,38 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int
+	int i = 0, j;
+	int **arr;
+
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
+	}
+	arr = (int **)malloc(height * sizeof(int *));
+	if (!arr)
+	{
+		free(arr);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+		if (!arr[i])
+		{
+			for (j = 0; j <= i; j++)
+			{
+				free(arr[j]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
 }
