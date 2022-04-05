e092aacf0bde0cc760924406bed8b87e07dea3b2
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-04-05 23:03:38 +0200

    Create 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,10 +28,10 @@ char *str_concat(char *s1, char *s2)
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		*(s + i) = *(s1 + i);
-	for (j = len1; *s2++ != '\0'; j++)
-		*(s + i + j) = *(s2 + j - len1);
+	for (j = len1; j < (len1 +len2); j++)
+		*(s + (i + j)) = *(s2 + (j - len1));
 
 	return (s);
 
