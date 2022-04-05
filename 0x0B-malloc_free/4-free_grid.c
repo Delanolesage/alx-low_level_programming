2b73a9913782121abd01b5da519b031f04092b8f
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-04-06 01:29:00 +0200

    Create 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,38 +12,11 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i = 0, j;
-	int **arr;
+	int i;
 
-	if (width <= 0 || height <= 0)
-	{
-		return (NULL);
-	}
-	arr = (int **)malloc(height * sizeof(int *));
-	if (!arr)
-	{
-		free(arr);
-		return (NULL);
-	}
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *)malloc(width * sizeof(int));
-		if (!arr[i])
-		{
-			for (j = 0; j <= i; j++)
-			{
-				free(arr[j]);
-			}
-			free(arr);
-			return (NULL);
-		}
+		free(grid[i]);
 	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
-	}
-	return (arr);
+	return (grid);
 }
