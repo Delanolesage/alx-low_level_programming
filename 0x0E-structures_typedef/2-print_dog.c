70d11d6978d09b12bd3257cf0c5ae07c6ae07043
Enzo Ahononga <52923737+Delanolesage@users.noreply.github.com>
2022-04-12 18:18:17 +0200

    Create 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,11 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-	    if ((*d).name != NULL)
-		    printf("Name: %s\n", (*d).name);
-	    else
-		    printf("Name: (nil)\n");
-	    printf("Age: %.6f\n", (*d).age);
-	    printf("Owner: %s", (*d).owner);
+		if ((*d).name != NULL)
+			printf("Name: %s\n", (*d).name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %.6f\n", (*d).age);
+		printf("Owner: %s", (*d).owner);
 	}
 }
