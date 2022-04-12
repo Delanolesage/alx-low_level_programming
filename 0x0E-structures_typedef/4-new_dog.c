276b5ff8b405843d9ca459be41dc7fa0819bb034
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-12 21:32:09 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,13 +10,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dog;
+	int i, j, z;
+	dog_t *newDog;
 
-	if (name != NULL && owner != NULL)
+	for (i = 0; name[i] != '\0'; i++)
+
+	for (j = 0; owner[j] != '\0'; j++)
+
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
 	{
-		dog = init_dog(name, age, owner);
-		return (dog);
-	}
-	else
+		free(newDog);
 		return (NULL);
+	}
+	newDog->name = malloc(i * sizeof(newDog->name));
+	if (newd->name == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+	for (z = 0; z <= i; z++)
+	{
+		(*newDog).name[z] = name[z];
+	}
+	(*newDog).age = age;
+	newDog->owner = malloc(j * sizeof(newDog->owner));
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+	for (z = 0; z <= j; z++)
+	{
+		newDog->owner[z] = owner[z];
+	}
+	return (newDog);
 }
