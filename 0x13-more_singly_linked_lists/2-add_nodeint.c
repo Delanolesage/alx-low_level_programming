1981293f8b86c3255d44e4afe5b9732e0e61b6d3
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-27 20:37:14 +0200

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,23 +7,20 @@
  * @n: int element of the linked list
  * Return: number of elements
  */
-
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new_list;
 
-	new_node = (listint_t *) malloc(sizeof(listint_t));
+	new_list = malloc(sizeof(listint_t));
 
-	if (new_node == NULL)
-	{
-		free(new_node);
+	if (new_list == NULL)
 		return (NULL);
-	}
 
-	new_node->n = n;
 
-	new_node->next = *head;
-	*head = new_node;
+	new_list->n = n;
+	new_list->next = *head;
+	*head = new_list;
 
-	return (*new_node);
+
+	return (*head);
 }
