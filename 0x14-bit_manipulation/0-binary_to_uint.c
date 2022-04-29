cbedc481660f45d7e0359efd09eff4737c29c83d
Delanolesage <52923737+Delanolesage@users.noreply.github.com>
2022-04-29 17:55:43 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,68 +1,68 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - find the length of a string
- * @s: pointer to the string to check
- * Return: void
-*/
-
-
-int _strlen(char *s)
-{
-int i = 0;
-while (s[i])
-	i++;
-
-return (i);
-}
-
-
-/**
- * _pow_recursion - Search a string for any of a set of bytes.
- * @x: base
- * @y: exposant
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
- */
-
-int _pow_recursion(int x, int y)
-{
-
-if (y < 0)
-	return (-1);
-else if (y == 1)
-	return (x);
-else if (y == 0)
-	return (1);
-
-return (x * _pow_recursion(x, y - 1));
-
-}
-
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: binary number
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: b is pointing to a string of 0 and 1 chars
  *
- * Return: 0 or converted number
+ * Return: unsigned integer, converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int i, len;
+	unsigned int temp = 0, sum = 0;
+	int i, len = 0;
 
 	if (b == NULL)
 		return (0);
-	len = _strlen(b);
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] == '1')
-			n += _pow_recursion(2, (len - i - 1));
-		else if (b[i] != '0')
-			return (0);
-	}
 
-	return (n);
+	len = _strlen(b);
+	i = 0;
+	while (b[i] != '\0')
+	{
+		if ((b[i] != '0' && b[i] != '1'))
+			return (0);
+		temp = (b[i] - 48) * _pow_recursion(2, (len - 1) - i);
+		sum += temp;
+		i++;
+	}
+	return (sum);
+}
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: parameter defined in main
+ *
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
+ * _pow_recursion - function that returns x raised to the power of y
+ * @x: parameter defined in main, integer
+ * @y: parameter defined in main, exponent
+ *
+ * Return: integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
